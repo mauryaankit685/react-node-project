b94a60160212cc33bc73f345917b2a814b8184fb
Ankit Maurya <mauryaankit685@gmail.com>
2026-03-28 18:47:30 +0530

    playing with the route

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,9 +5,25 @@ const app = express();
 
 // order of route is matter 
 
+
+
+app.get("/user/:userid/:pass", (req, res) => { // /user/777
+    console.log(req.params)
+
+    res.send("Hello from user to get user id")
+})
+
 app.get("/user", (req, res) => {
+    console.log(req.query)
+    // res.send(req.query)
     res.send("Hello from user")
 })
+
+
+//  will both /ac or /abc  -> just like regular expressions * + all are can add and we can add regex directly
+// app.get("/ab?c", (req, res) => {
+//     res.send("Hello from user")
+// })
 
 app.post("/user", (req, res) => {
     res.send("user is post")
