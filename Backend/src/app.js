6009b52420df263c9529f45983f83411cdbf6c7a
Ankit Maurya <mauryaankit685@gmail.com>
2026-03-29 18:30:15 +0530

    error heandling recommended to use try catch

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,22 +3,42 @@ const app = express();
 const { adminAuth, userAuth } = require('./middlewares/auth')
 // middleware is a checkpoint that is checking/modifing/stoping before reaching the request to the final api
 
-app.use('/admin', adminAuth)
 
-app.get('/user', userAuth, (req, res) => {
-    // middleware
-    res.send('user is here')
+
+app.get('/userData', (req, res) => {
+    throw new Error("hdfkadas");
+    res.send('userData')
 })
 
-app.get('/admin/user', (req, res) => {
-    // middleware
-    res.send('data set successfully')
+app.use('/', (err, req, res, next) => {
+    if (err) {
+        console.log(err)
+        res.status(500).send("somthing went wrong")
+
+    }
 })
 
-app.delete('/admin/remove', (req, res) => {
-    // middleware
-    res.send('data deleted successfully')
-})
+
+
+// app.use('/admin', adminAuth)
+
+// app.get('/user', userAuth, (req, res) => {
+//     // middleware
+//     res.send('user is here')
+// })
+
+// app.get('/admin/user', (req, res) => {
+//     // middleware
+//     res.send('data set successfully')
+// })
+
+// app.delete('/admin/remove', (req, res) => {
+//     // middleware
+//     res.send('data deleted successfully')
+// })
+
+
+
 
 
 app.listen("3001", () => {
